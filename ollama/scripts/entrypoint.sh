15ee1a3fdeef181e9aa1543e78d0c9f029b2d27f
Mateusz Kurowski <m.kurowski.023@studms.ug.edu.pl>
2025-06-11 00:49:41 +0200

    remove redundant compose

--- ollama/scripts/entrypoint.sh
+++ ollama/scripts/entrypoint.sh
@@ -10,7 +10,8 @@ echo "🔴 Retrieve model..."
 # ollama pull devstral:24b # 16 GB VRAM
 # ollama pull deepseek-r1:8b # 6 GB VRAM
 # ollama pull qwen3:8b # 6 GB VRAM
-ollama pull gemma3:1b # 1 GB VRAM (cpu possible)
+# ollama pull gemma3:1b # 1 GB VRAM (cpu possible)
+ollama pull qwen3:0.6b # 0.6 GB VRAM (cpu possible)
 
 echo "🟢 Done pulling"
 
